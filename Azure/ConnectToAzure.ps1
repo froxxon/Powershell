9a8d7d0f2eef8c61fc732ac66f09cf60e61b6e61
froxxon <froxxen@gmail.com>
2019-02-12 15:17:57 +0100

    Add files via upload

---
+++ Azure/ConnectToAzure.ps1
@@ -0,0 +1,13 @@
+﻿import-module msonline
+#get-command -module msonline
+
+$msolcred = get-credential "" # <- Enter username for Azure
+connect-msolservice -credential $msolcred
+
+$SyncedUsers = get-msoluser -MaxResults 10
+$AzureUsers = get-msoluser -MaxResults 10
+
+$SyncedUsers.Count
+$AzureUsers.Count
+
+$SyncedUsers[1] | fl
