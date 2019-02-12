e0a596918aca4b943ac13a2cd3bd2deeae8717a3
froxxon <froxxen@gmail.com>
2019-02-12 15:10:31 +0100

    Add files via upload

---
+++ Active Directory/Get-DeletedObjects.ps1
@@ -0,0 +1,8 @@
+﻿$date = New-Object System.DateTime(2017,2,20)
+$Users = get-adobject -filter {(createTimeStamp -gt $date) -and (deleted -eq $True)} -IncludeDeletedObjects -properties *
+Clear-Host
+samAccountName
+ForEach ( $User in $Users ) {
+    If ( $User.objectClass -eq "groupPolicyContainer" ) { "$($User.DisplayName)`n$($User.objectClass)`n$($User.Modified)`n" ; Continue }
+    "$($User."msDS-LastKnownRDN")`n$($User.objectClass)`n$($User.Modified)`n"
+}
