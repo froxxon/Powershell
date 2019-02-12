446f0e0ae80b77869891fde2a6b9ea8babe080bd
froxxon <froxxen@gmail.com>
2019-02-12 15:31:55 +0100

    Add files via upload

---
+++ AD-Delegation/Set-RolesInfo.ps1
@@ -0,0 +1,26 @@
+﻿$LookupDomains = @("domain1","domain2")
+
+$AllRoles = @()
+ForEach ( $LookupDomain in $LookupDomains ) {
+    $DomainDN = $(Get-ADDomain -Server $LookupDomain).Distinguishedname
+    $Roles = Get-ADGroup -filter 'Name -like "Role-T*"' -SearchBase "OU=Admin,$DomainDN" -SearchScope Subtree -Server $LookupDomain -Properties Name, Info | Select Name, Info
+    ForEach ( $Role in $Roles ) {
+        If ( $AllRoles.Name -notcontains $Role.Name ) { $AllRoles += $Role }
+    }
+}
+$AllRoles = $AllRoles | Sort Name
+
+ForEach ( $LookupDomain in $LookupDomains ) {
+    If ( $LookupDomain -ne "domain3" ) {
+        ForEach ( $Role in $AllRoles ) {
+            Try{  
+                Try {
+                    Set-ADGroup $Role.Name -replace @{info="Manager: $Manager"} -Server $LookupDomain
+                    Write-host "Sets Info for group: $($Role.Name) in domain: $LookupDOmain"
+                }
+                Catch {}
+            }
+            Catch {}
+        }
+    }
+}
