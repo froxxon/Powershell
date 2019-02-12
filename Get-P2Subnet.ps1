f8a0d8b712a3ad4cf1d60df4a25581b34a22e9d4
froxxon <froxxen@gmail.com>
2019-02-12 14:34:58 +0100

    Add files via upload

---
+++ Get-P2Subnet.ps1
@@ -0,0 +1,9 @@
+﻿clear-host
+Import-Module 2PintFunctions -Force
+write-host $(Get-Date)
+
+Get-StiflerSubnet -Server w002142 -Property LocationName, ActiveClients, AverageBandwidth, SubnetID |
+    Where ActiveClients -gt 0 |
+    Select LocationName, SubnetID, ActiveClients, AverageBandwidth, RedLeader, BlueLeader |
+    Sort AverageBandwidth, LocationName -Descending |
+    Format-Table -AutoSize
