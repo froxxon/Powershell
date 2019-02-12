31d5e3d389dcb7c8696f4691737a62d8fa633b64
froxxon <froxxen@gmail.com>
2019-02-12 15:28:27 +0100

    Add files via upload

---
+++ AD-Delegation/Get-Tier2UsersWithoutRole.ps1
@@ -0,0 +1,16 @@
+﻿$DomainDN = $(Get-ADDomain).DistinguishedName
+$Tier2Users = $(Get-ADUser -filter * -SearchBase "OU=T2-Accounts,OU=Tier 2,OU=Admin,$DomainDN" -SearchScope OneLevel).Name
+
+ForEach ( $User in $L2Users ) {
+    $ConnectedToRule = $False
+    $memberOf = $(Get-ADPrincipalGroupMembership $User).Name
+    ForEach ( $Member in $MemberOf ) {
+        If ( $Member -like "Role-*" ) { 
+            $ConnectedToRule = $True
+            Continue
+        }
+    }
+    If ( $ConnectedToRule -eq $False ) {
+        Write-host "$User"
+    }
+}
