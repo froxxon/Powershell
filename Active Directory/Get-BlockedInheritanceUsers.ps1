5530641dd9102b481d8f1c7ed4ca21d919b002b4
froxxon <froxxen@gmail.com>
2019-02-12 15:12:26 +0100

    Add files via upload

---
+++ Active Directory/Get-BlockedInheritanceUsers.ps1
@@ -0,0 +1,17 @@
+﻿$Containers = @()
+$UserStatuses = @()
+
+"Reading OU List ..."
+$Containers = Get-ADOrganizationalUnit -Filter * -Properties * | sort canonicalname | select distinguishedname, canonicalname
+
+"Reading Container List ..."
+$Containers += Get-ADObject -SearchBase (Get-ADDomain).distinguishedname -SearchScope OneLevel -LDAPFilter '(objectClass=container)' -Properties * | sort canonicalname | select distinguishedname, canonicalname
+
+foreach($Cntr in $Containers)
+{
+    "Evaluating - " + $Cntr.distinguishedname + " ..."
+    
+    $UserStatuses += Get-ADUser -Filter * -SearchBase $Cntr.distinguishedname -SearchScope OneLevel -Properties * | where {($_.nTSecurityDescriptor.AreAccessRulesProtected -eq $true) -and ($_.enabled -eq $true)} | select @{n='OU';e={$Cntr.distinguishedname}}, displayname, userprincipalname,samAccountName, @{n='Inheritance Broken';e={$_.nTSecurityDescriptor.AreAccessRulesProtected}}
+}
+
+$UserStatuses | export-csv -path C:\temp\UsersWithInheritanceBroken.csv
