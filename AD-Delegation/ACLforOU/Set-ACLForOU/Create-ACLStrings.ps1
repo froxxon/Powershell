c79901e3578da8083be004bef28561592b25d2ba
froxxon <froxxen@gmail.com>
2019-02-12 15:24:18 +0100

    Add files via upload

---
+++ AD-Delegation/ACLforOU/Set-ACLForOU/Create-ACLStrings.ps1
@@ -0,0 +1,10 @@
+﻿import-module "C:\temp\sharedcode.psm1"
+$ACLs = get-content "C:\temp\Get-ACLForOU\Get-ACLForOU.log"
+$Logfile = "C:\temp\set-aclforou\Created ACL strings.txt"
+
+ForEach ( $ACL in $ACLs ) {
+    $Group = $($ACL -split ";")[0]
+    $OU = $($ACL -split ";")[1]
+    $ACL = $ACL.TrimStart("$Group;$OU;")
+    Write-Log "Set-ACLForOU -Group ""$Group"" -OU ""$OU"" -AccessRights ""$ACL""" -WritePrefix No
+}
