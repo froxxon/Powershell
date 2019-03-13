382b2a4c6b555b7cb577f0aed720a9c89829c404
Fredrik Bergman <20445155+froxxon@users.noreply.github.com>
2019-03-14 00:08:19 +0100

    Add files via upload

---
+++ SCCM/Add-SiteSystemServer_To_BoundaryGroup.ps1
@@ -0,0 +1,15 @@
+﻿Clear-Host
+Import-Module "C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\ConfigurationManager.psd1"
+cd a01:
+$Subnets = @("Bound01")
+$SiteSystemServer = @("server01.domain.local","server02.domain.local")
+
+ForEach ( $Subnet in $Subnets ) {
+    Try {
+        Set-CMBoundaryGroup -Name "Production - Central content - $Subnet" -AddSiteSystemServerName $SiteSystemServer
+        Write-host "Added servers to boundary group for subnet $Subnet"
+    }
+    Catch {
+        Write-host "Failed to add servers to boundary group for subnet $Subnet"
+    }
+}
---
+++ SCCM/Set-Boundaries_To_Peer-BoundaryGroup.ps1
@@ -0,0 +1,29 @@
+﻿Clear-Host
+Import-Module "C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\ConfigurationManager.psd1"
+cd a01:
+$Boundaries = @("Bound01","Bound02")
+
+ForEach ( $Boundary in $Boundaries ) {
+    Try {
+        $BoundaryID = $(Get-CMBoundary -BoundaryName "*Production - Central - $Boundary*").BoundaryID
+        Write-host "Successfully" -ForegroundColor Green -NoNewline ; Write-host " retrieved BoundaryID: $BoundaryID for Boundary: ""*Production - Central - $Boundary*"""
+        Try {
+            Add-CMBoundaryToGroup -BoundaryID $BoundaryID -BoundaryGroupName "Production - Central Peer content"
+            Write-host "Successfully" -ForegroundColor Green -NoNewline ; Write-host " added BoundaryGroup: ""Production - Central Peer content"" to BoundaryID: $BoundaryID"
+            Try {
+                Remove-CMBoundaryFromGroup -BoundaryGroupName "Production - Central content - $Boundary" -BoundaryId $BoundaryID -Force
+                Write-host "Successfully" -ForegroundColor Green -NoNewline ; Write-host "Successfully removed BoundaryID: $BoundaryID from BoundaryGroup: ""Production - Central content  - $Boundary"""
+            }
+            Catch {
+                Write-host "Failed" -ForegroundColor Red -NoNewline ; Write-host " to remove BoundaryID: $BoundaryID from BoundaryGroup: ""Production - Central content - $Boundary"""
+            }
+        }
+        Catch {
+            Write-host "Failed" -ForegroundColor Red -NoNewline ; Write-host " to add BoundaryGroup: ""Production - Central content"" to BoundaryID: $BoundaryID"
+        }
+    }
+    Catch {
+        Write-host "Failed" -ForegroundColor Red -NoNewline ; Write-host " to retrieve BoundaryID: $BoundaryID for Boundary: ""*Production - Central - $Boundary*"""
+    }
+    Write-host ""
+}
