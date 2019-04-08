7d7583a8103df80d9dd700f7bcf1260e23e942d9
Fredrik Bergman <20445155+froxxon@users.noreply.github.com>
2019-04-08 23:58:25 +0200

    Add files via upload

---
+++ 2Pint/Get-P2Subnet.ps1
@@ -0,0 +1,12 @@
+﻿clear-host
+Import-Module 2PintFunctions -Force
+
+Get-StiflerSubnet *U* -Server StiflerServer.domain.local -Property LocationName, ActiveClients, AverageBandwidth, SubnetID | Where ActiveClients -gt 0 | Select LocationName, SubnetID, ActiveClients, AverageBandwidth, RedLeader | Sort LocationName | Format-Table -AutoSize
+
+$(Get-StiflerSubnet *U* -Server StiflerServer.domain.local).LocationName | Sort
+
+Get-StiflerSubnet MySubnet -Server StiflerServer.domain.local -ShowRedLeader
+
+Get-StiflerSubnet -SubnetID 192.168.20.0 -Server StiflerServer.domain.local -ShowRedLeader
+
+get-stiflerclient -Client CLI- -Server StiflerServer.domain.local
